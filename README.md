25fbcf4d83b2113ca1bf7418a9ef1d154fb60573
freewind <nowindlee@gmail.com>
2018-08-09 16:50:35 +0800

    demo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Kotlin Ktor Demo
-================
+Kotlin Ktor Send Html File Demo
+===============================
 
-Use [ktor](http://ktor.io) to write web server.
+Use [ktor](http://ktor.io) to send static html file to client.
 
 Run `Hello.kt` in your IDE, and open <http://localhost:8080> in your browser.
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,18 +1,18 @@
 package example
 
 import io.ktor.application.call
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
+import io.ktor.response.respondFile
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import java.io.File
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, port = 8080) {
         routing {
             get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
+                call.respondFile(File("./static/index.html"))
             }
         }
     }
