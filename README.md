ac6f074261b53805e55b5cf39f56755c559cf472
freewind <nowindlee@gmail.com>
2018-08-09 14:44:55 +0800

    demo

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Kotlin Hello World Demo
-=======================
+Kotlin Ktor Demo
+================
+
+Use [ktor](http://ktor.io) to write web server.
 
 Hello world from kotlin. Open it in IDE and run `Hello.kt`
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,7 +1,20 @@
 package example
 
-fun main(args: Array<String>) {
-    println(hello("Kotlin"))
-}
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-fun hello(name: String): String = "Hello, $name!"
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+        }
+    }
+    server.start(wait = true)
+}
